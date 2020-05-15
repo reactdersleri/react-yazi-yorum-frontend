f7a2e24b7afd04d28ab4de0154f3f8b3732a7cb0
reactdersleri <reactdersleri@gmail.com>
2020-05-15 01:40:29 -0700

    Yorum listesi ve yorum ekleme formu eklendi

--- src/components/YaziDetayi.js
+++ src/components/YaziDetayi.js
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import YaziYorumlari from "./YaziYorumlari";
 
 const YaziDetayi = (props) => {
   const { id } = props.match.params;
   const [yaziDetayi, setYaziDetayi] = useState({});
+  const [yorumlar, setYorumlar] = useState([]);
+
+  const handleCommentSubmit = (event, yorum) => {
+    event.preventDefault();
+    axios
+      .post(
+        `https://react-yazi-yorum.herokuapp.com/posts/${id}/comments`,
+        yorum
+      )
+      .then((response) => {
+        setYorumlar([...yorumlar, response.data]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     axios
-      .get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`)
-      .then((response) => {
-        setYaziDetayi(response.data);
+      .all([
+        axios.get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`),
+        axios.get(
+          `https://react-yazi-yorum.herokuapp.com/posts/${id}/comments`
+        ),
+      ])
+      .then((responses) => {
+        setYaziDetayi(responses[0].data);
+        setYorumlar(responses[1].data);
       })
       .catch((error) => {
         console.log(error);
@@ -21,6 +44,7 @@ const YaziDetayi = (props) => {
       <h2 className="ui header">{yaziDetayi.title}</h2>
       <p>{yaziDetayi.created_at}</p>
       <p>{yaziDetayi.content}</p>
+      <YaziYorumlari yorumlar={yorumlar} handleSubmit={handleCommentSubmit} />
     </React.Fragment>
   );
 };
