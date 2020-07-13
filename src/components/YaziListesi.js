c36f2c3246e543e31aa91af00b7b10855a51d693
reactdersleri <reactdersleri@gmail.com>
2020-07-12 17:43:50 -0700

    console.log check for strictmode

--- src/components/YaziListesi.js
+++ src/components/YaziListesi.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 const YaziListesi = (props) => {
   const [yaziListesi, setYaziListesi] = useState([]);
 
+  console.log({ yaziListesi });
+
   useEffect(() => {
     api()
       .get("/posts")
